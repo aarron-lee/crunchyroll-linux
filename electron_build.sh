9c59448b17e50b1666d1fbac0b97f650c0f35956
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-08-11 11:04:02 -0400

    merge upstream profiles changes

--- electron_build.sh
+++ electron_build.sh
@@ -9,9 +9,5 @@ cp -rf img/ electron/static/build
 cp -rf js/ electron/static/build
 cp -rf css/ electron/static/build
 
-# use android auth
-# auth: "Basic eHVuaWh2ZWRidDNtYmlzdWhldnQ6MWtJUzVkeVR2akUwX3JxYUEzWWVBaDBiVVhVbXhXMTE=",
-sed -i 's/auth: "Basic [^"]*",/auth: "Basic eHVuaWh2ZWRidDNtYmlzdWhldnQ6MWtJUzVkeVR2akUwX3JxYUEzWWVBaDBiVVhVbXhXMTE=",/' electron/static/build/server/js/core/service.js
-
 # add game controller listenser
 sed -i 's/<!-- end js -->/<script src="server\/js\/electron\/controllerListener.js"><\/script>/g' electron/static/build/index.html
